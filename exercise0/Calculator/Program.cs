6f351a2b441632604a1442d9e9584d6dc2f70bc1
mmmpedersen <202006450@post.au.dk>
2022-09-08 11:03:43 +0200

    HaandTest

--- exercise0/Calculator/Program.cs
+++ exercise0/Calculator/Program.cs
@@ -8,8 +8,15 @@ namespace exercise0
 {
     internal class Program
     {
+       
         static void Main(string[] args)
         {
+        Calculator TestCalc;
+        TestCalc = new Calculator();
+        System.Console.WriteLine("Expected result is 4:\n Actual result is: {0}", TestCalc.Add(2.0, 2.0));
+        System.Console.WriteLine("\nExpected result is 3:\n Actual result is: {0}", TestCalc.Subtract(6.0, 3.0));
+
         }
+
     }
 }
