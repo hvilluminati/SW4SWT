b807677aa44343590efbf937016b0f76ddf6aa1f
hvilluminati <Hvillum.magnus@gmail.com>
2022-09-08 10:46:15 +0200

    added actual files

--- exercise0/Calculator/Class1.cs
+++ exercise0/Calculator/Class1.cs
@@ -4,9 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Calculator
+namespace exercise0
 {
-    internal class Class1
+    public class Calculator
     {
+        public Calculator()
+        { }
+
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Power(double x, double exponent)
+        {
+
+            return Math.Pow(x, exponent);
+        }
     }
 }
