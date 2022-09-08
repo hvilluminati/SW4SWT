bcca1aaf4969ca91848c290cbde78fb7e3222ffe
mmmpedersen <202006450@post.au.dk>
2022-09-08 11:19:37 +0200

    HaandTestDone

--- exercise0/Calculator/Program.cs
+++ exercise0/Calculator/Program.cs
@@ -14,8 +14,19 @@ namespace exercise0
         Calculator TestCalc;
         TestCalc = new Calculator();
         System.Console.WriteLine("Expected result is 4:\n Actual result is: {0}", TestCalc.Add(2.0, 2.0));
-        System.Console.WriteLine("\nExpected result is 3:\n Actual result is: {0}", TestCalc.Subtract(6.0, 3.0));
+        System.Console.WriteLine("Expected result is -4:\n Actual result is: {0}", TestCalc.Add(-2.0, -2.0));
 
+        System.Console.WriteLine("\nExpected result is 3:\n Actual result is: {0}", TestCalc.Subtract(6.0, 3.0));
+        System.Console.WriteLine("\nExpected result is -9:\n Actual result is: {0}", TestCalc.Subtract(-6.0, 3.0));
+
+        System.Console.WriteLine("\nExpected result is 16:\n Actual result is: {0}", TestCalc.Multiply(8.0, 2.0));
+        System.Console.WriteLine("\nExpected result is -16:\n Actual result is: {0}", TestCalc.Multiply(-8.0, 2.0));
+        System.Console.WriteLine("\nExpected result is 0:\n Actual result is: {0}", TestCalc.Multiply(-8.0, 0.0));
+
+        System.Console.WriteLine("\nExpected result is 8:\n Actual result is: {0}", TestCalc.Power(2.0, 3.0));
+        System.Console.WriteLine("\nExpected result is -8:\n Actual result is: {0}", TestCalc.Power(-2.0, 3.0));
+        System.Console.WriteLine("\nExpected result is 0.125:\n Actual result is: {0}", TestCalc.Power(2.0, -3.0));
+        System.Console.WriteLine("\nExpected result is 1:\n Actual result is: {0}", TestCalc.Power(2.0, 0.0));
         }
 
     }
