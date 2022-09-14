c0bc9fa8c4a1b226a002ee02db14e0103c228398
hvilluminati <Hvillum.magnus@gmail.com>
2022-09-14 14:50:05 +0200

    Update UnitTest1.cs

--- exercise0/Calculator.Test.Unit/UnitTest1.cs
+++ exercise0/Calculator.Test.Unit/UnitTest1.cs
@@ -16,21 +16,31 @@ namespace Calculator.Test.Unit
         public void AddTest()
         {
             Assert.That(uut.Add(6, 4), Is.EqualTo(10));
+            Assert.That(uut.Add(-6, 4), Is.EqualTo(-2));
+            Assert.That(uut.Add(-6, -4), Is.EqualTo(-10));
         }
         [Test]
         public void SubtractTest()
         {
             Assert.That(uut.Subtract(4, 4), Is.EqualTo(0));
+            Assert.That(uut.Subtract(-4, 4), Is.EqualTo(-4));
+            Assert.That(uut.Subtract(4, -4), Is.EqualTo(8));
+            Assert.That(uut.Subtract(-4, -4), Is.EqualTo(0));
         }
         [Test]
         public void MultiplyTest()
         {
             Assert.That(uut.Multiply(4, 4), Is.EqualTo(16));
+            Assert.That(uut.Multiply(-4, 4), Is.EqualTo(-16));
+            Assert.That(uut.Multiply(-4, -4), Is.EqualTo(16));
         }
         [Test]
         public void PowerTest()
         {
             Assert.That(uut.Power(4, 4), Is.EqualTo(256));
+            Assert.That(uut.Power(-4, 4), Is.EqualTo(-256));
+            Assert.That(uut.Power(4, -4), Is.EqualTo(0.00390625));
+            Assert.That(uut.Power(-4, -4), Is.EqualTo(-0.00390625));
         }
     }
 }
